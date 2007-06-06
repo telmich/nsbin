b7a34a34299209dca6cb87e509243767bbad0d9e
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-06-06 11:42:10 +0200

    On screen display using defined text and repeats it

---
+++ werbeosd.sh
@@ -0,0 +1,9 @@
+file=wordlist
+
+
+while true; do 
+   while read satz; do
+      echo "$satz" | osd_cat -p middle -A center -f '-misc-fixed-medium-r-semicondensed--*-542-*-*-c-*-*-*' -d 1
+      sleep 23
+   done < "$file"
+done
