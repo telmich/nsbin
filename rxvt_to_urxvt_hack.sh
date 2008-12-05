74b4b65fcab7bcedf67e08f8a6243e513f5e62d9
Nico Schottelius <nico@ikn.ethz.ch>
2008-12-05 15:39:34 +0100

    add the old (u)rxvt hack
    Signed-off-by: Nico Schottelius <nico@ikn.ethz.ch>

---
+++ rxvt_to_urxvt_hack.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#===============================================================================
+# Author: Nico Schottelius
+# Created: 2007-01-04
+# Description: rxvt is known to most systems, urxvt not. Hack it.
+#==============================================================================
+
+echo '[ "$TERM" = "rxvt-unicode" ] && export TERM=rxvt' >> "$HOME/.profile"
+echo '[ "$TERM" = "rxvt-unicode" ] && export TERM=rxvt' >> "$HOME/.bash_profile"
+echo 'if ( $TERM == rxvt-unicode ) setenv TERM rxvt' >> "$HOME/.cshrc"
