6959074b57aa79f9d78cbf1d3dd54825070ae70d
Nico Schottelius <nico@ikn.schottelius.org>
2009-01-30 12:28:47 +0100

    eth wlan needs to sleep
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

--- eth-wlan-vpn.sh
+++ eth-wlan-vpn.sh
@@ -1,8 +1,18 @@
 #!/bin/sh -e
 # Nico Schottelius, 20080910 11:09
-ip link set wlan0 down
-iwconfig wlan0 essid public
-ip link set wlan0 up
-iwconfig wlan0 essid public
+#ip link set wlan0 down
+#iwconfig wlan0 essid public
+#ip link set wlan0 up
+#iwconfig wlan0 essid public
+
+set -x
+
+( wpa_cli terminate; exit 0 )
+sleep 2
+wpa_supplicant -B -Dwext -iwlan0 -c ~nico/ethz/wlan/wpa_supplicant.conf
+sleep 5
 udhcpc -nqfi wlan0
-vpnc ~nico/firmen/ethz/vpn/vpnc.conf
+vpnc ~nico/ethz/vpn/pc.conf
+
+#vpnc ~nico/firmen/ethz/vpn/vpnc.conf
+# vpnc-disconnect
