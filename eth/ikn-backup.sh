7995b5202bac29cb27affdf3a456163f6fc498ec
Nico Schottelius <nico@ikn.schottelius.org>
2009-04-17 19:59:01 +0200

    updated ikn-backup to use power of ccollect
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

--- eth/ikn-backup.sh
+++ eth/ikn-backup.sh
@@ -1,11 +1,16 @@
 #!/bin/sh -e
 
-cryptsetup luksOpen /dev/sdc usbhd
-mount /home/services/usbhd 
 
-cd /home/user/nico/p/ccollect/ccollect
-CCOLLECT_CONF=/home/services/usbhd/ccollect ./ccollect.sh wöchentlich ikn
+ccollect=/home/user/nico/p/ccollect/ccollect/ccollect.sh
 
-umount /home/services/usbhd 
-cryptsetup luksClose usbhd
+# in pre_exec
+#cryptsetup luksOpen /dev/sdc usbhd
+#mount /home/services/usbhd 
+
+#cd /home/user/nico/p/ccollect/ccollect
+CCOLLECT_CONF=/home/users/nico/ethz/ccollect $ccollect wöchentlich ikn
+
+# in post_exec
+#umount /home/services/usbhd 
+#cryptsetup luksClose usbhd
 
