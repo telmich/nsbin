a2108100a0c2c65a920e6165321349f6284bdda4
Nico Schottelius <nico@ikn.schottelius.org>
2009-10-21 13:47:59 +0200

    print out version of ikiwiki
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

--- ikiwikitest.sh
+++ ikiwikitest.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -x
 BDIR=~/b/ikiwiki
+
+( cd "${BDIR}" && git describe )
+
 PERL5LIB=${BDIR}  ${BDIR}/ikiwiki.in \
 	--libdir ${BDIR} \
 	--templatedir ${BDIR}/templates \
 	-set underlaydir=${BDIR}/underlays \
 	-set underlaydirbase=${BDIR}/underlays \
 	--setup ikiwiki.setup \
-	--refresh
+   "$@"
+echo ikiwiki result: $?
