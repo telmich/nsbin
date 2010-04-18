f012426604111f8ad97d103345c410bf8a0fc30c
Nico Schottelius <nico@ikn.schottelius.org>
2010-04-18 15:11:45 +0200

    add script to init new git repo on sans
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

---
+++ eth/sans-git-init.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# 
+# 2008      Nico Schottelius (nico-nsbin at schottelius.org)
+# 
+# This file is part of nsbin.
+#
+# nsbin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# nsbin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with nsbin. If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Usage: $0 repo pseudo-owner
+#
+
+basedir=/home/services/sans/git
+group=sans
+
+repo="$1"; shift
+powner="$1"; shift
+
+export GIT_DIR="${basedir}/${repo}"
+
+git init
+touch "${GIT_DIR}/git-daemon-export-ok"
+cat << eof > "${GIT_DIR}/config"
+[core]
+        filemode = true
+        bare = true
+        sharedRepository = group
+[gitweb]
+        owner = "${powner:-Unknown stranger}"
+eof
+
+chgrp sans -R "${GIT_DIR}"
+chmod g+rw -R "${GIT_DIR}
